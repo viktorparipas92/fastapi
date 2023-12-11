a3f73e13cbfae02e00e51e2e7c67abaa0f2cb897
Paripás Viktor <viktor.paripas@kognity.com>
2023-12-11 17:56:52 +0100

    Add required query param to item detail

--- tutorial/main.py
+++ tutorial/main.py
@@ -32,9 +32,12 @@ async def read_items(skip: int = 0, limit: int = 10):
 
 @app.get('/items/{item_id}')
 async def read_item(
-    item_id: int, query: Optional[str] = None, short: bool = False
+    item_id: int,
+    needy: str,
+    query: Optional[str] = None,
+    short: bool = False,
 ):
-    item = {'item_id': item_id}
+    item = {'item_id': item_id, 'needy': needy}
     if query:
         item['query'] = query
 
